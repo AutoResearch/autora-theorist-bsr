ac4d25fe7e7843deff12e715193f6d68775ca660
benwandrew <benwallaceandrew@gmail.com>
2023-09-21 11:06:06 -0400

    ci: pushing local pre-commit changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
-import pytest
 import random
 
-@pytest.fixture(autouse=True, scope='session')
+import pytest
+
+
+@pytest.fixture(autouse=True, scope="session")
 def set_seed():
     """
     Note that in BSR we use random functions from the random module instead of np.random
